d4fbb42998fbb1ea1873a016ba49b6dcebc32d4c
SUYASH AGNIHOTRI <suyashagnihotri988@gmail.com>
2024-03-20 20:13:55 +0530

    third commit

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -32,7 +32,7 @@ const Explore = () => {
  
     const shouldShowSearchResults = searchValue !== "";
     const shouldShowPosts = !shouldShowSearchResults && 
-      posts.pages.every(( item ) => item.documents.length === 0);
+      posts.pages.every(( item ) => item?.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -73,7 +73,7 @@ const Explore = () => {
           />
         ): shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
-        ) :( posts.pages.map((item, index) => (
+        ) :( posts.pages.map((item: any, index) => (
           <GridPostList key={`page-${index}`} posts={item.documents} />
         )
           ))}
