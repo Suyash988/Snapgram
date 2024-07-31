2e14151ef7e7223fbc005445a2208b81f01dea4b
SUYASH AGNIHOTRI <suyashagnihotri988@gmail.com>
2024-07-31 14:41:17 +0530

    fourth commit
    ignore the typescript errors during the build process

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -158,20 +158,20 @@ export const useDeletePost = () => {
 }
 
 export const useGetPosts = () => {
-  // return useInfiniteQuery({
-    // queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    // queryFn: getInfinitePosts,
-    // getNextPageParam: (lastPage) => {
-    //   // If there's no data, there are no more pages.
-    //   if (lastPage && lastPage.documents.length === 0) {
-    //     return null;
-    //   }
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
+      // If there's no data, there are no more pages.
+      if (lastPage && lastPage.documents.length === 0) {
+        return null;
+      }
 
-    //   // Use the $id of the last document as the cursor.
-    //   const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
-    //   return lastId;
-    // },
-  // });
+      // Use the $id of the last document as the cursor.
+      const lastId = lastPage?.documents[lastPage.documents.length - 1].$id;
+      return lastId;
+    },
+  });
 };
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-export default defineConfig({
+export default defineConfig(
+  {
   plugins: [react()],
   resolve: {
     alias: {
